260d16164be44114b6319ba7d6ec9e51772ef8d8
James Goff <james.goff1030@gmail.com>
2022-04-14 15:05:29 -0700

    solved problem 8

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -123,8 +123,12 @@ let sumNum = myFavoriteNumbers.slice(4,5)
 
 //Code Here
 if(myFavoriteNumbers.length < 7){
-  console.log("there are not enough elements in this array")
+console.log("there are not enough elements in this array")
+} else{ 
+  someNum = myFavoriteNumbers[6];
 }
+
+
 
 
 
@@ -141,6 +145,13 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 // Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
 
 //Code Here
+for(let i = 0; i <= listOfNumbers.length; i++){
+  if(listOfNumbers[i] % 3 === 0){
+  console.log(`${listOfNumbers[i]} is divisble by 3`)
+  }
+}
+
+
 
 
 
